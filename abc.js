379ee7463d09d02a81737a69911d919bf16d8110
axihh <axihh134@gmail.com>
2025-04-04 14:40:24 +0800

    修改abc文件

--- abc.js
+++ abc.js
@@ -1,4 +1,4 @@
-console.log("abc")
+console.log("abc扶摇直上九万里")
 /**
  * github远程仓库操作方式一：
  * git init -> git add . -> git commit -m xxx -> git remote add 别名 地址 -> git branch --set-upstream-to=别名/分支 -> git pull -> git push 别名 本地分支/head:远程分支(github远程仓库操作比较特殊 如果上下游分支不同名的话 那么push时需要明确指定别名+head+分支)
